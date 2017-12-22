c55d307fe6b14acb2ae24379d9ebe944625ae0ee
Ryan T. Hosford <tad.hosford@gmail.com>
2017-12-21 21:27:17 -0600

    Further simplify 'Fractal-art' solution

--- 21/fart.rb
+++ 21/fart.rb
@@ -4,14 +4,16 @@ class Fart
   attr_accessor :program
   def initialize(file)
     @rules = File.read(file).scan(/([.\/#]*) => ([.\/#]*)/).to_h
-    @program = (<<-EOS).rstrip
-.#.
-..#
-###
-    EOS
+    @program = ".#.\n..#\n###"
   end
 
   def call
+    subprograms      = locate_subprograms(program)
+    expanded_program = expand_subprograms(subprograms)
+    @program         = assemble_expanded_program(expanded_program)
+  end
+
+  def locate_subprograms(program)
     split_program = program.split(/[\n\/]/)
     if split_program.size.even?
       subprogram_indices = 0.upto(split_program.size - 1).select(&:even?)
@@ -20,57 +22,53 @@ class Fart
       subprogram_indices = 0.upto(split_program.size - 1).to_a.select {|v| v % 3 == 0 }
       subprograms = subprogram_indices.map {|j| subprogram_indices.map {|i| "#{split_program[j][(i)..(i+2)]}/#{split_program[j+1][(i)..(i+2)]}/#{split_program[j+2][(i)..(i+2)]}" } }.flatten
     end
-
-    expanded_program = subprograms.map {|p| transpose_program(p) }
-
-    if expanded_program.size == 1
-      @program = expanded_program[0]
-    else
-      root = Math.sqrt(expanded_program.join.gsub("/", "").size)
-
-      factor = if expanded_program.size.even?
-        3
-      else
-        case root % 2
-        when 0
-          4
-        else
-          3
-        end
-      end
-      @program = expanded_program.map {|x| x.split("/") }.each_slice(root / factor).to_a.map(&:transpose).map {|grp| grp.map(&:join) }.map {|x| x.join("\n") }.join("\n")
-    end
   end
 
-  def rules_array
-    @rules.keys.map {|x| x.split("/").map {|str| str.split(//)} }
+  def expand_subprograms(subprograms)
+    subprograms.map {|p| transpose_program(p) }
+  end
+
+  def assemble_expanded_program(expanded_program)
+    # First run needs no reassembly.
+    return expanded_program[0] if expanded_program.size == 1
+
+    # Subsequent runs produce expanded versions of many subprograms,
+    # these will need reassembly.
+    root     = Math.sqrt(expanded_program.join.gsub("/", "").size)
+    factor   = expanded_program.size.even? || root % 2 == 1 ? 3 : 4
+
+    expanded_program.map {|x| x.split("/") }.each_slice(root / factor).to_a.map(&:transpose).map {|grp| grp.map(&:join) }.map {|x| x.join("\n") }.join("\n")
   end
 
   def transpose_program(program)
     rulified = program.rstrip.gsub("\n", "/")
 
-    @rules.fetch(rulified) {
+    @rules[rulified] = @rules.fetch(rulified) {
       if double_rotated_rule = @rules.keys.detect {|x| x.split("/").reverse.map(&:reverse).join("/") == rulified }
-        @rules[rulified] = @rules[double_rotated_rule]
+        @rules[double_rotated_rule]
       elsif rotated_and_flipped_rule = rules_array.map(&:transpose).map(&:reverse).detect {|r| r.map {|x| x.join }.join("/") == rulified }
-        @rules[rulified] = @rules[rotated_and_flipped_rule.reverse.transpose.map {|r| r.join }.join("/")]
+        @rules[rotated_and_flipped_rule.reverse.transpose.map {|r| r.join }.join("/")]
       elsif flipped_and_rotated_rule = rules_array.map(&:reverse).map(&:transpose).detect {|r| r.map {|x| x.join }.join("/") == rulified }
-        @rules[rulified] = @rules[flipped_and_rotated_rule.transpose.reverse.map {|r| r.join }.join("/")]
+        @rules[flipped_and_rotated_rule.transpose.reverse.map {|r| r.join }.join("/")]
       elsif flipped_rule = rules_array.map(&:reverse).detect {|r| r.map {|x| x.join }.join("/") == rulified }
-        @rules[rulified] = @rules[flipped_rule.reverse.map {|r| r.join }.join("/")]
+        @rules[flipped_rule.reverse.map {|r| r.join }.join("/")]
       elsif magic_rule = @rules.keys.detect {|k| k.split("/").map(&:chars).transpose.map(&:join).join("/").reverse == program }
-        @rules[rulified] = @rules[magic_rule]
+        @rules[magic_rule]
       elsif rotated_rule = rules_array.map(&:transpose).detect {|r| r.map {|x| x.join }.join("/") == rulified }
-        @rules[rulified] = @rules[rotated_rule.transpose.map {|r| r.join }.join("/")]
+        @rules[rotated_rule.transpose.map {|r| r.join }.join("/")]
       else
         raise rulified
       end
     }
   end
+
+  def rules_array
+    @rules.keys.map {|x| x.split("/").map {|str| str.split(//)} }
+  end
 end
 
 fart = Fart.new("input.txt")
 puts "PART 2 (runtime): #{Benchmark.measure { 18.times { fart.call } }}"
-puts "PART 2 (results): #{fart.program.chars.count {|c| c == "#"}}"
+puts "PART 2 (results):   #{fart.program.chars.count {|c| c == "#"}}"
 
 File.open('PART2.txt', 'w') { |file| file.write(fart.program) }
