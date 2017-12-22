d4cee5eb1bb8b96d6b814ddae38af6cb9df06264
Ryan T. Hosford <tad.hosford@gmail.com>
2017-12-22 00:17:27 -0600

    Add visualization code

---
+++ 22/sporifica_to_png.rb
@@ -0,0 +1,37 @@
+require 'oily_png'
+
+class SporificaToPng
+  def initialize width = 414
+		@width  = width
+		@height = width
+
+    @grid = File.readlines("2017-day22-part2.txt").map(&:rstrip)
+    @grid = @grid.map(&:chars)
+  end
+
+  def step
+  end
+
+  def draw
+    img = ChunkyPNG::Image.new(@width, @height, ChunkyPNG::Color::TRANSPARENT)
+
+    @grid.each.with_index {|row,y|
+      row.each.with_index {|char,x|
+        img[x,y] = case char
+        when "."
+          :aliceblue
+        when "W"
+          :darksalmon
+        when "#"
+          :maroon
+        when "F"
+          :lightsalmon
+        end
+      }
+    }
+
+    img.save('2017-d22-part2.png', :interlace => true)
+  end
+end
+
+FartToPng.new.draw
